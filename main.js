1c4b7152683c5fb33cb416fd56c8e52a96bec58e
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-05-30 15:06:15 +0200

    okay, thats the best i can do

--- main.js
+++ main.js
@@ -1,48 +1,60 @@
-async function getStoko() {
-    const data = await fetch('./stoko.json');
-    const stoko = await data.json();
+window.addEventListener('DOMContentLoaded', (event) => {
+    goApe();
+});
 
-    const dijo = Object.keys(stoko);
+const setTheStore = async (store) => {
+    const storeHTML = `
+    <dt class="name">
+        <h1>
+            <a href="/"><img src="${store.logo}" /></a>
+            ${store.name}
+        </h1>
+    </dt>
+    <dd>${store.description}</dd>
+    <dd class="address">${store.address}</dd>
+    <dd class="phone"><a href="tel:${store.phone}">${store.phone}</dd>
+    `;
+    document.getElementById('store').innerHTML = storeHTML;
+    document.getElementById('store').classList.remove('ping');
+};
 
-    let listOfGroups = '';
+const stockTheStore = async (stoko) => {
+    const dijo = document.getElementById('dijo');
+    dijo.classList.remove('ping');
 
-    dijo.forEach((foodGroup) => {
-        let listOfItems = '';
-
-        stoko[foodGroup].forEach((foodItem) => {
-            if (foodItem.available === true) {
-                const itemHTML = `
-                <li>
+    stoko.forEach((foodGroup, index) => {
+        const group = document.createElement('div');
+        group.classList.add('food-group', foodGroup.type);
+        const groupItemsList = document.createElement('ul');
+        foodGroup.options.forEach((foodItem) => {
+            const item = document.createElement('li');
+            item.classList.add('food-item', `${foodItem.name}`);
+            item.innerHTML = `
+                <a href="#${foodGroup.type}-${foodItem.name}" class="picture pulse">
+                    <img src="${foodItem.picture}" class="pulse"  />
+                </a>
+                <div class="details">
                     <h3 class="name">${foodItem.name}</h3>
-                    <a href="#starch-rice" class="picture"> 
-                        <img src="${foodItem.picture}" width="200px" height="200px" /> 
-                    </a>
-                    <span class="price">R${foodItem.price}</span>
+                    <strong class="price">R${foodItem.price},</strong>
                     <span class="quantity">${foodItem.weight}g</span>
-                </li>
-                `;
-                listOfItems += itemHTML;
-            }
+                </div>
+            `;
+            groupItemsList.appendChild(item);
+            // item.querySelector('a').style.backgroundImage()
+            item.querySelector('img').addEventListener('load', (e) => {
+                e.target.parentElement.classList.remove('pulse');
+            });
+            //console.log(item.querySelector('img'));
         });
 
-        const groupHTML = `
-            <div class="food-group">
-                <ul>
-                ${listOfItems}
-                </ul>
-            </div>
-            `;
-
-        console.log(foodGroup, groupHTML);
-        listOfGroups += groupHTML;
+        group.appendChild(groupItemsList);
+        dijo.append(group);
     });
+};
 
-    const cupboard = document.querySelector('#stoko');
-    cupboard.innerHTML = listOfGroups;
-
-    cupboard.classList.remove('ping');
-}
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    getStoko();
-});
+const goApe = async () => {
+    const data = await fetch('./data.json');
+    const stoko = await data.json();
+    await setTheStore(stoko.store);
+    await stockTheStore(stoko.dijo);
+};
